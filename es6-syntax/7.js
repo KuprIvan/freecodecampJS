4da09cfd2b15afd38e40a2553035cae450251c1b
ivankupryianau <ivankupryianau@gmail.com>
2023-01-16 19:56:51 +0100

    feat: add some tasks

--- es6-syntax/7.js
+++ es6-syntax/7.js
@@ -1,10 +1,9 @@
-function quickCheck(arr, elem) {
-  // Only change code below this line
-  if (arr.indexOf(elem) !== -1) {
-    return true
-  }
-  return false
-  // Only change code above this line
-}
-
-console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+let myNestedArray = [
+  // change code below this line
+  ["unshift", false, 1, 2, 3, "complex", "nested"],
+  ["loop", "shift", 6, 7, 1000, "method"],
+  ["concat", false, true, "spread", "array", ["deep"]],
+  ["mutate", 1327.98, "splice", "slice", "push", [["deeper"]]],
+  ["iterate", 1.3849, 7, "8.4876", "arbitrary", "depth", [[["deepest"]]]]
+  // change code above this line
+];
